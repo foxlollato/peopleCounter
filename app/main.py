128833bdb3f68480d7f4542e10db08e0a3c4c439
foxlollato <vitor.lollato@gmail.com>
2019-10-30 08:16:15 -0300

    ident

--- app/main.py
+++ app/main.py
@@ -24,10 +24,8 @@ def saida():
 @main.route('/')
 def index():
     entradas = Detect.query.filter_by(detect_type='entrada').count()
-    saidas = Detect.query.filter_by(detect_type='saida').count()
-    
-    quantidade =  entradas - saidas
-
+    saidas = Detect.query.filter_by(detect_type='saida').count()    
+    quantidade =  entradas - saidas    
     return render_template('index.html', quantidade=quantidade)
 
 @main.route('/relatorio')
@@ -38,18 +36,15 @@ def relatorio():
 def relatorio_post():
     date_format = '%Y-%m-%dT%H:%M'
     startDate = datetime.strptime(request.form.get('date-start'), date_format)
-    endDate = datetime.strptime(request.form.get('date-end'), date_format)    
-    
+    endDate = datetime.strptime(request.form.get('date-end'), date_format)
     visitors = db.session.query(Detect).filter(Detect.detect_date.between(startDate, endDate)).all()
     
     if startDate >= endDate:
-        print('a')
         flash('Data de fim deve ser mais recente que a data de início.')
         return render_template('relatorio.html')
 
     if len(visitors) == 0:
         flash('Sem dados no período.')
         return render_template('relatorio.html')
-    
 
     return render_template('relatorio.html', visitors=visitors)
