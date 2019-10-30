2c8658fa8b3c1f33fb96814f15930d71cae7025e
foxlollato <vitor.lollato@gmail.com>
2019-10-30 08:10:43 -0300

    add Detect model class

--- app/models.py
+++ app/models.py
@@ -5,4 +5,3 @@ class Detect(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     detect_type = db.Column(db.String(10))
     detect_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
-
