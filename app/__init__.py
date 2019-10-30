5ddd7ba6646961fd8ea7311b1644ee60ec5e9346
foxlollato <vitor.lollato@gmail.com>
2019-10-30 18:21:51 -0300

    add flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
-
+# from . import models
 db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/tmp/db.sqlite3'
     app.config['SECRET_KEY'] = '12345abcde'
+    
     db.init_app(app)
+    from .models import User, Detect
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
