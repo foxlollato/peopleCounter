9a86ce822e6335d4a8fb84d8d0fba2b340f9cb8f
foxlollato <vitor.lollato@gmail.com>
2019-10-30 08:09:50 -0300

    add report controller

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request, flash
 from . import db
+from datetime import datetime
 from .models import Detect
+
 main = Blueprint('main', __name__)
 
 @main.route('/entrada')
@@ -21,11 +23,33 @@ def saida():
 
 @main.route('/')
 def index():
-    visitors = Detect.query.all()
-
     entradas = Detect.query.filter_by(detect_type='entrada').count()
     saidas = Detect.query.filter_by(detect_type='saida').count()
     
     quantidade =  entradas - saidas
 
-    return render_template('index.html', visitors=visitors, quantidade=quantidade)
+    return render_template('index.html', quantidade=quantidade)
+
+@main.route('/relatorio')
+def relatorio():
+    return render_template('relatorio.html')
+
+@main.route('/relatorio', methods=['POST'])
+def relatorio_post():
+    date_format = '%Y-%m-%dT%H:%M'
+    startDate = datetime.strptime(request.form.get('date-start'), date_format)
+    endDate = datetime.strptime(request.form.get('date-end'), date_format)    
+    
+    visitors = db.session.query(Detect).filter(Detect.detect_date.between(startDate, endDate)).all()
+    
+    if startDate >= endDate:
+        print('a')
+        flash('Data de fim deve ser mais recente que a data de início.')
+        return render_template('relatorio.html')
+
+    if len(visitors) == 0:
+        flash('Sem dados no período.')
+        return render_template('relatorio.html')
+    
+
+    return render_template('relatorio.html', visitors=visitors)
