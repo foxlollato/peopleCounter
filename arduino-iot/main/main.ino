651c4a70ddd7bf83070a879e75de6bb706d34a73
foxlollato <vitor.lollato@gmail.com>
2019-11-11 16:04:33 -0300

    set wireless network and adjus delays

--- arduino-iot/main/main.ino
+++ arduino-iot/main/main.ino
@@ -5,6 +5,11 @@ const byte txPin = 3;
 const byte sIn = 5;
 const byte sOut = 6;
 
+const String server="SERVER_IP";
+const String port="PORT";
+const String ssid="SSID";
+const String password="PASSWORD";
+
 SoftwareSerial ESP8266 (rxPin, txPin);
 
 void setup() {
@@ -12,18 +17,20 @@ void setup() {
   pinMode(sOut, INPUT);
   Serial.begin(9600);   
   ESP8266.begin(9600);
+  delay(5000);
+  ESP8266.println("AT+CWJAP=\""+ssid+"\",\""+password+"\"");  
 }
 
 void exec(String action){
   String cmd = "GET /"+action+" HTTP/1.1";
-  ESP8266.println("AT+CIPSTART=\"TCP\",\"192.168.0.112\",8081");
-  delay(150);   
-  ESP8266.println("AT+CIPSEND=" + String(cmd.length() + 4));
-  delay(150);
+  ESP8266.println("AT+CIPSTART=\"TCP\",\""+server+"\","+port);
+  delay(10);
+  ESP8266.println("AT+CIPSEND=" + String(cmd.length()+4));
+  delay(10);
   ESP8266.println(cmd);
-  delay(150);
+  delay(10);
   ESP8266.println("");
-  delay(1000);
+  delay(250);
 }
 
 void loop() {
