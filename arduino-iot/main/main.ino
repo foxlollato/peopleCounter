19d281da3c1f7be02147e2b3aa7f1df72f84bf7b
foxlollato <vitor.lollato@gmail.com>
2019-11-11 16:17:17 -0300

    set wireless network and adjus delays

--- arduino-iot/main/main.ino
+++ arduino-iot/main/main.ino
@@ -1,47 +1,64 @@
 #include <SoftwareSerial.h>
 
+//set ESP8266 wireless controller pins
 const byte rxPin = 2;
 const byte txPin = 3;
+
+//set in sensor pin
 const byte sIn = 5;
+//set out sensor pin
 const byte sOut = 6;
 
+//server and wireless settings
 const String server="SERVER_IP";
 const String port="PORT";
 const String ssid="SSID";
 const String password="PASSWORD";
 
+//instantiate serial object to communicate with esp8266 over pins rxPin txPin
 SoftwareSerial ESP8266 (rxPin, txPin);
 
 void setup() {
+  //set sensor pins for input mode
   pinMode(sIn, INPUT);
   pinMode(sOut, INPUT);
+  //begin serial communication: "Serial" for Arduino console and "ESP8266" for wireless controller
   Serial.begin(9600);   
   ESP8266.begin(9600);
+  //Delay to avoid missconfiguration for wireless setup
   delay(5000);
   ESP8266.println("AT+CWJAP=\""+ssid+"\",\""+password+"\"");  
 }
 
 void exec(String action){
+  //Message format to API
   String cmd = "GET /"+action+" HTTP/1.1";
+  //start tcp communication from esp8266 to server
   ESP8266.println("AT+CIPSTART=\"TCP\",\""+server+"\","+port);
   delay(10);
+  //send message length
   ESP8266.println("AT+CIPSEND=" + String(cmd.length()+4));
   delay(10);
+  //send message
   ESP8266.println(cmd);
   delay(10);
+  //send blank character to end message
   ESP8266.println("");
   delay(250);
 }
 
 void loop() {
-
+    //"listening" sIn value LOW
     if(digitalRead(sIn) == LOW){
       delay(10);
+      //if sensor blocked, wait unblock
       while(digitalRead(sIn) == LOW){
         delay(10);
-      }
+      }//then, call exec function and pass action according sensor
       exec("entrada");
     }
+
+    //same as above
     if(digitalRead(sOut) == LOW){
       delay(10);
       while(digitalRead(sOut) == LOW){
@@ -49,8 +66,8 @@ void loop() {
       }
       exec("saida");
     }
-
-  if (ESP8266.available()) {
-    Serial.write(ESP8266.read());
-  }
+  //uncomment to get some log from esp8266
+  // if (ESP8266.available()) {
+  //   Serial.write(ESP8266.read());
+  // }
 }
